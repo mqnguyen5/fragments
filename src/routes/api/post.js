406e1af43941f6da29b3f7c9f35b0ef80f04c30b
Luke Nguyen <lukenguyen.dev@gmail.com>
2022-10-05 12:45:05 -0400

    Implement POST /v1/fragments: - Update `src/routes/api/index/js` to use express raw body parser - Use raw body parser as middleware for POST route - Add implementation for POST /v1/fragments - Add unit tests - Add logging

---
+++ src/routes/api/post.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const { Fragment } = require('../../model/fragment');
+const logger = require('../../logger');
+const { createSuccessResponse } = require('../../response');
+
+module.exports = async (req, res, next) => {
+  try {
+    const data = req.body;
+    const contentType = req.headers['content-type'];
+
+    logger.debug(`Validating fragment's media type, got ${contentType}`);
+    if (!Buffer.isBuffer(data)) {
+      logger.warn(`${contentType} is not supported, throwing 415 error`);
+      const error = new Error(`Media type is not supported, got ${contentType}`);
+      error.status = 415;
+      throw error;
+    } else {
+      logger.info(`Attempting to create fragment and set fragment's data`);
+      const fragment = new Fragment({
+        ownerId: req.user,
+        type: contentType,
+      });
+      await fragment.save();
+      await fragment.setData(data);
+
+      logger.debug(`Fragment created successfully, got metadata ${JSON.stringify(fragment)}`);
+      res.setHeader('Location', `${process.env.API_URL}/v1/fragments/${fragment.id}`);
+      res.status(201).json(createSuccessResponse({ fragment }));
+    }
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ tests/unit/post.test.js
@@ -0,0 +1,75 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+const hash = require('../../src/hash');
+
+describe('POST /v1/fragments', () => {
+  test('unauthenticated requests are denied', () => request(app).post('/v1/fragments').expect(401));
+
+  test('incorrect credentials are denied', () =>
+    request(app).post('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
+
+  test('authenticated users can create a plain text fragment', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('This is a fragment');
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body.status).toBe('ok');
+  });
+
+  test(`response fragment's metadata includes id, ownerId, created, updated, type, and size`, async () => {
+    const contentType = 'text/plain';
+    const data = 'This is a fragment';
+
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', contentType)
+      .send(data);
+
+    expect(res.body.fragment).toEqual(
+      expect.objectContaining({
+        id: expect.stringMatching(
+          /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/
+        ),
+        ownerId: hash('user1@email.com'),
+        created: expect.stringMatching(/^(\d{4}|\d{6})-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$$/),
+        updated: expect.stringMatching(/^(\d{4}|\d{6})-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/),
+        type: contentType,
+        size: Buffer.byteLength(data),
+      })
+    );
+  });
+
+  test('responses include a Location header with a URL to GET', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('This is a fragment');
+
+    expect(res.headers).toHaveProperty(
+      'location',
+      `http://localhost:8080/v1/fragments/${res.body.fragment.id}`
+    );
+  });
+
+  test('creating a fragment with an unsupported type is denied', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'application/msword')
+      .send('This is a fragment');
+
+    expect(res.status).toEqual(415);
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        status: 'error',
+        error: expect.objectContaining({ code: 415 }),
+      })
+    );
+  });
+});
